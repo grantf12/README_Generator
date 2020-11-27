fdc85f0d1ad5322317bb15c5edf5adfa2ef31ae4
Grant <grantferment@gmail.com>
2020-11-27 14:51:41 -0500

    test

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# README_Generator
+{
+	"Project Title: ": "g",
+	"Project Description: ": "f",
+	"Installation:": "d",
+	"Usage Info": "s",
+	"Contribution Guidelines: ": "a",
+	"Test Instructions: ": ""
+}
--- index.js
+++ index.js
@@ -1,15 +1,55 @@
+const inquirer = require("inquirer");
+const fs = require('fs')
+
 // array of questions for user
 const questions = [
-
-];
+{
+    type: 'input',
+    message: 'Enter the Project Name: ',
+    name: 'Project Title: '
+},
+{
+    type: 'input',
+    message: 'Enter a description of the project: ',
+    name: 'Project Description'
+},
+{
+    type: 'input',
+    message: 'Enter the installation instructions: ',
+    name: 'Installation'
+},
+{
+    type: 'input',
+    message: 'Enter the usage information: ',
+    name: 'Usage Info'
+},
+{
+    type: 'input',
+    message: 'Enter contribution guidelines: ',
+    name: 'Contribution Guidelines'
+},
+{
+    type: 'input',
+    message: 'Enter test instructions: ',
+    name: 'Test Instructions'
+}];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), err =>
+    err ? console.log(err) : console.log('Success!')
+    )
 }
 
 // function to initialize program
 function init() {
-
+inquirer
+    .prompt (
+        questions
+    )
+    .then((response) => {
+        writeToFile("README.md", response)
+    })
 }
 
 // function call to initialize program
